e72daf964453faa8375fff35baf69eceae273516
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-08-05 03:40:38 +0600

    middleware added.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
+import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
+import cors from "cors"
+import config from "./config";
 
 const app: Application = express();
+
+app.use(cors({
+    origin:config.app_url,
+    credentials:true
+}))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
