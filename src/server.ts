1e2ee97ca7cb1d30b957fa8cea856f7c43d61b31
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-08-05 03:14:20 +0600

    Setting Up server.ts, app.ts, files with express

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import app from "./app"
+import "dotenv/config";
+import { prisma } from "./lib/prisma"
 
-const port = 3000
+const port = process.env.PORT
 
 async function main() {
     try {
+        // await prisma.$connect();
+        console.log('Connected to the database successfully')
         app.listen(port, () => {
             console.log(`Example app listening on port ${port}`)
         })
     } catch (error) {
         console.log(error)
+        // await prisma.$disconnect();
     }
 }
 main();
