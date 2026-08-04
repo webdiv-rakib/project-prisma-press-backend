799fafe07b910d2c12b982a56d6d3f97deebe2f7
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-08-05 03:29:28 +0600

    implemented environment variable.

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,14 @@
 import app from "./app"
 import "dotenv/config";
 import { prisma } from "./lib/prisma"
-
-const port = process.env.PORT
+import config from "./config";
 
 async function main() {
     try {
         // await prisma.$connect();
         console.log('Connected to the database successfully')
-        app.listen(port, () => {
-            console.log(`Example app listening on port ${port}`)
+        app.listen(`${config.port}`, () => {
+            console.log(`Example app listening on port ${config.port}`)
         })
     } catch (error) {
         console.log(error)
