b43d633bc3200d3afe3845f54087e39fa7f374ac
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-08-12 23:40:26 +0600

    Implementing Login User API

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,18 @@
+import bcrypt from "bcryptjs"
+import { prisma } from "../../lib/prisma"
 import { IloginUser } from "./auth.interface"
 
-const loginUser = (payload:IloginUser) => {
-    
+const loginUser = async (payload: IloginUser) => {
+    const { email, password } = payload
+    const user = await prisma.user.findUniqueOrThrow({
+        where: { email }
+    })
+
+    const isPasswordMatched = await bcrypt.compare(password, user.password)
+    if (!isPasswordMatched) {
+        throw new Error('Incorrent Password')
+    }
+    return user
 
 }
 export const authService = {
