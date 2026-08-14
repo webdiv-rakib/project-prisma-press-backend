87e68bae10e7d07398c6c36b84f04f7671f86a0a
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-08-14 18:38:17 +0600

    role base authorization organized.

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,64 @@
+import { NextFunction, Request, Response } from "express";
+import { Role } from "../../generated/prisma/enums";
+import { catchAsync } from "../utils/catchAsync";
+import { jwtUtils } from "../utils/jwt";
+import config from "../config";
+import { JwtPayload } from "jsonwebtoken";
+import { prisma } from "../lib/prisma";
+
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: {
+                email: string,
+                name: string,
+                id: string,
+                role: Role
+            }
+        }
+    }
+}
+const auth = (...requiredRoles: Role[]) => {
+    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.cookies.accessToken ? req.cookies.accessToken :
+            req.headers.authorization?.startsWith("Bearer") ? req.headers.authorization?.split(" ")[1] : req.headers.authorization
+        if (!token) {
+            throw new Error("You are not logged in. Please lon in to access this resource")
+        }
+        const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret);
+        if (!verifiedToken.success) {
+            throw new Error(verifiedToken.error)
+        }
+        const { email, name, id, role } = verifiedToken.data as JwtPayload
+        if (requiredRoles.length && !requiredRoles.includes(role)) {
+            throw new Error("Forbidden.You don't have permission to access this resource")
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id,
+                email,
+                name,
+                role
+            }
+        })
+
+        if (!user) {
+            throw new Error("User not found")
+        }
+        if (user.activeStaus === "BLOCKED") {
+            throw new Error("Your account has been blocked. Please contact support")
+        }
+
+        req.user = {
+            email,
+            name,
+            id,
+            role
+        }
+
+        next();
+    })
+}
+export default auth;
--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -1,73 +1,11 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import { userController } from './user.controller';
-import { jwtUtils } from "../../utils/jwt";
-import config from "../../config";
 import { Role } from "../../../generated/prisma/enums";
-import httpStatus from 'http-status';
-import { catchAsync } from "../../utils/catchAsync";
-import { JwtPayload } from "jsonwebtoken";
-import { prisma } from "../../lib/prisma";
+import auth from "../../middlewares/auth";
 
 const router = Router();
 
-declare global {
-    namespace Express {
-        interface Request {
-            user?: {
-                email: string,
-                name: string,
-                id: string,
-                role: Role
-            }
-        }
-    }
-}
-
 router.post('/register', userController.createUser);
-
-const auth = (...requiredRoles: Role[]) => {
-    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-        const token = req.cookies.accessToken 
-        // || req.headers.authorization?.startsWith("Bearer") ? req.headers.authorization?.split(" ")[1] : req.headers.authorization
-        if (!token) {
-            throw new Error("You are not logged in. Please lon in to access this resource")
-        }
-        const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret);
-        if (!verifiedToken.success) {
-            throw new Error(verifiedToken.error)
-        }
-        const { email, name, id, role } = verifiedToken.data as JwtPayload
-        if (requiredRoles.length && !requiredRoles.includes(role)) {
-            throw new Error("Forbidden.You don't have permission to access this resource")
-        }
-
-        const user = await prisma.user.findUnique({
-            where: {
-                id,
-                email,
-                name,
-                role
-            }
-        })
-
-        if (!user) {
-            throw new Error("User not found")
-        }
-        if (user.activeStaus === "BLOCKED") {
-            throw new Error("Your account has been blocked. Please contact support")
-        }
-
-        req.user = {
-            email,
-            name,
-            id,
-            role
-        }
-
-        next();
-    })
-}
-
 
 router.get('/me', auth(Role.ADMIN, Role.USER, Role.AUTHOR),
     //     (req: Request, res: Response, next: NextFunction) => {
