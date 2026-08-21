1601d6eddf609a6114aa890b4a72cfa7362ce7c9
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-08-21 18:16:54 +0600

    Update comment.service.ts

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -9,3 +9,4 @@ export const commentService = {
 // i went to home for janaza
 // tomorrow is my final examination
 // i have not finished my sylabus yet
+// tomorrow is my another exam than i will start working on my assignment
