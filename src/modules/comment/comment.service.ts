8c701a93b7d5eaaf6cbb6cd655600f9652c26e1b
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-08-23 20:51:28 +0600

    Update comment.service.ts

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -4,9 +4,3 @@ const commentIntoDB = async () => {
 export const commentService = {
     commentIntoDB
 }
-
-// cha cha died today at morning
-// i went to home for janaza
-// tomorrow is my final examination
-// i have not finished my sylabus yet
-// tomorrow is my another exam than i will start working on my assignment
