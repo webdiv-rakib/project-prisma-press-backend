f4f624d194de6b0f453550e611380cb8d7e01e17
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-09-18 00:13:02 +0600

    Understanding The Scenario For Transaction And Rollback

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,3 +1,4 @@
+import { CommentStatus } from "../../../generated/prisma/enums";
 import { prisma } from "../../lib/prisma"
 import { ICreatePostPayload, IUpdatePostPayload } from "./post.interface"
 
@@ -29,13 +30,13 @@ const getAllPosts = async () => {
 
 // get post by id from database
 const getPostsById = async (postId: string) => {
-    const post = await prisma.post.findUniqueOrThrow({
-        where: {
-            id: postId
-        }
-    })
+    // const post = await prisma.post.findUniqueOrThrow({
+    //     where: {
+    //         id: postId
+    //     }
+    // })
 
-    const updatedPost = await prisma.post.update({
+    await prisma.post.update({
         where: {
             id: postId
         },
@@ -44,16 +45,43 @@ const getPostsById = async (postId: string) => {
                 increment: 1
             }
         },
+        // include: {
+        //     author: {
+        //         omit: {
+        //             password: true
+        //         }
+        //     },
+        //     comments: true
+        // }
+    })
+
+    const post = await prisma.post.findUniqueOrThrow({
+        where: {
+            id: postId
+        },
         include: {
             author: {
                 omit: {
                     password: true
                 }
             },
-            comments: true
+
+            comments: {
+                where: {
+                    status: CommentStatus.APPROVED
+                },
+                orderBy: {
+                    createdAt: "desc"
+                }
+            },
+            _count: {
+                select: {
+                    comments: true
+                }
+            }
         }
     })
-    return updatedPost
+    return post
 }
 
 // update post by id
